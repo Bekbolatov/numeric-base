7d96e94029150e2e9db13382ff3812e88f7b1e0a
Renat Bekbolatov <renatbek@gmail.com>
2014-09-19 13:28:52 -0700

    added logging

--- app/com/sparkydots/starpractice/services/Authenticator.scala
+++ app/com/sparkydots/starpractice/services/Authenticator.scala
@@ -1,10 +1,11 @@
 package com.sparkydots.starpractice.services
 
 import java.security.MessageDigest
-import play.api.mvc.{RequestHeader, Results, EssentialAction, Action}
 
-
+import play.api.Logger
 import play.api.http.HeaderNames._
+import play.api.libs.ws.WS
+import play.api.mvc.{Action, EssentialAction, RequestHeader, Results}
 
 import scala.concurrent.ExecutionContext
 
@@ -12,6 +13,9 @@ import scala.concurrent.ExecutionContext
  * @author Renat Bekbolatov (renatb@sparkydots.com) 9/18/14 9:14 PM
  */
 case class Authenticator(action: EssentialAction) extends EssentialAction with Results {
+  val starLogger = Logger("star")
+  val sep = " # "
+
   val digest = MessageDigest.getInstance("MD5")
   def md5(s: String) = digest.digest(s.getBytes).map("%02x".format(_)).mkString
   def md5check(hash: String, orig: String) =
@@ -19,16 +23,26 @@ case class Authenticator(action: EssentialAction) extends EssentialAction with R
 
   def apply(rh: RequestHeader) = {
     implicit val executionContext: ExecutionContext = play.api.libs.concurrent.Execution.defaultContext
+    val ip =  rh.remoteAddress
+    val path = rh.path
+    val ua = rh.headers.get("User-Agent").getOrElse("")
     val authorization = rh.headers.get(AUTHORIZATION).getOrElse("")
+
     authorization.split(":") match {
       case Array(secret, public, version) =>
         val check = md5check(public, secret)
         if (check) {
+          starLogger.info(s"$ip$sep$path$sep$public$sep$version$sep$ua")
           action(rh)
         } else {
-          Action { request => Ok("{}") } (rh)
+          starLogger.info(s"$ip${sep}${path}NO:$sep$public$sep$version$sep$ua")
+          Action { request => Ok("{}")}(rh)
         }
-      case _ => Action { request => Ok("{}") } (rh)
+      case _ =>
+        starLogger.info(s"$ip$sep${path}NA${sep}NA$sep$ua")
+        Action { request => Ok("{}")}(rh)
     }
   }
+
+
 }
