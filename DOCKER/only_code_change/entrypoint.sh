a7606757aeda78c1f053684acde18282978006c5
Renat Bekbolatov <rbekbolatov@avvo.com>
2016-07-22 04:03:18 -0700

    add port number during reg

--- DOCKER/only_code_change/entrypoint.sh
+++ DOCKER/only_code_change/entrypoint.sh
@@ -5,7 +5,7 @@ ln -s /EFS/distrib/lib/* /deployment/lib/
 
 #register
 THIS_HOST=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
-touch /EFS/run/services/webserver/$THIS_HOST
+touch /EFS/run/services/webserver/$THIS_HOST:8080
 
 
 exec $@
